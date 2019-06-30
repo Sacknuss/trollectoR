989764c62d58f3d3e7f1787bf6ec76bab7cd8179
Dominic Hug <DominicHug@bluewin.ch>
2019-06-30 16:31:46 +0200

    matches metadata from ripped cd with metadata from musicbrainz

--- art.py
+++ art.py
@@ -1,24 +1,19 @@
 __author__ = 'Lucky Hooker'
 
 import config
-import metadata
 import os
 import simplejson
 import urllib.request
 
-root = config.root
 api_key_fanart = config.api_key_fanart
-
-url_artist = config.url_musicbrainz + 'artist/?query=%s&fmt=json'
-url_release_group = config.url_musicbrainz + 'release-group?artist=%s&type=album&fmt=json'
+root = config.root
 user_agent = {'User-Agent': 'TheCollector/0.0.1 ( anomalitaet@gmail.com )'}
 
 def exist_cd_art(path):
-
     counter = 0
     files = os.listdir(path)
-    for file in files:
 
+    for file in files:
         if 'cdart' in file:
             counter += 1
 
@@ -29,11 +24,10 @@ def exist_cd_art(path):
 
 
 def exist_cover_art(path):
-
     counter = 0
     files = os.listdir(path)
-    for file in files:
 
+    for file in files:
         if 'fanart' in file:
             counter += 1
 
@@ -43,75 +37,64 @@ def exist_cover_art(path):
         return False
 
 
-def get_disc_art(artist_id, release_id, path):
-
+def get_disc_art(artist_id, release_group_id, path):
     try:
-
         url = "http://webservice.fanart.tv/v3/music/%s?api_key=%s" % (artist_id, api_key_fanart)
         result = urllib.request.urlopen(url).read()
         data = simplejson.loads(result)
-        album = data['albums'][release_id]
+        album = data['albums'][release_group_id]
         cd_arts = album['cdart']
         counter = 0
 
         for cover in cd_arts:
-
             if counter < 1:
                 img_url = cover['url']
+                img_url = img_url.replace("https://", "http://")
                 urllib.request.urlretrieve(img_url, path + '/cdart.png')
             else:
                 img_url = cover['url']
+                img_url = img_url.replace("https://", "http://")
                 urllib.request.urlretrieve(img_url, path + '/cdart' + str(counter) + '.png')
-
             counter += 1
 
+    except urllib.error.HTTPError as e:
+        print(e.__dict__)
+    except urllib.error.URLError as e:
+        print(e.__dict__)
     except:
-
         print("No CD Art Found!")
 
 
-def get_cover_art(artist_id, release_id, path):
-
+def get_cover_art(artist_id, release_group_id, path):
     try:
-
         url = "http://webservice.fanart.tv/v3/music/%s?api_key=%s" % (artist_id, api_key_fanart)
         result = urllib.request.urlopen(url).read()
         data = simplejson.loads(result)
-        album = data['albums'][release_id]
+        album = data['albums'][release_group_id]
         covers = album['albumcover']
         counter = 0
 
         for cover in covers:
-
             if counter < 1:
                 img_url = cover['url']
+                img_url = img_url.replace("https://", "http://")
                 urllib.request.urlretrieve(img_url, path + '/fanart.jpg')
             else:
                 img_url = cover['url']
+                img_url = img_url.replace("https://", "http://")
                 urllib.request.urlretrieve(img_url, path + '/fanart' + str(counter) + '.jpg')
-
             counter += 1
 
+    except urllib.error.HTTPError as e:
+        print(e.__dict__)
+    except urllib.error.URLError as e:
+        print(e.__dict__)
     except:
-
         print("No Fan Art Found!")
 
 
-def get_cd_art(artist, album, path):
-
-    artists = metadata.get_artists_by_name(artist)
-
-    for artist_id, artist_name in artists.items():
-        
-        release_group = metadata.get_release_group_by_name(artist_id, album)
-        if len(release_group) > 0:
-
-            print("{'" + artist_name + "': '" + artist_id + "'}")
-            print(release_group)
-
-            for release_group_name, release_group_id in release_group.items():
-
-                if not exist_cover_art(path):
-                    get_cover_art(artist_id, release_group_id, path)
-                if not exist_cd_art(path):
-                    get_disc_art(artist_id, release_group_id, path)
+def get_cd_art(artist_id, release_group_id, path):
+    if not exist_cover_art(path):
+        get_cover_art(artist_id, release_group_id, path)
+    if not exist_cd_art(path):
+        get_disc_art(artist_id, release_group_id, path)
--- metadata.py
+++ metadata.py
@@ -1,142 +1,247 @@
 __author__ = 'Lucky Hooker'
 
+from art import get_cd_art
 import config
 import math
 import musicbrainzngs
-import mutagen
+from mutagen.flac import FLAC, Picture
 import os
 import pathlib
-import re
+import pprint
 
 root = config.root
-api_key_fanart = config.api_key_fanart
-
 musicbrainzngs.set_useragent('TheCollector','0.0.1','anomalitaet@gmail.com')
+pp = pprint.PrettyPrinter(indent=10)
 
-def create_list_complete():
-
+def get_local_artist_list(artist):
     cd_list = []
-    artists = os.listdir(root)
-    artists.sort()
+    records = os.listdir(root + '/' + artist)
+    records.sort()
 
-    for artist in artists:
+    for record in records:
+        record_dir = os.path.join(root + '/' + artist + '/' + record)
 
-        artist_dir = os.path.join(root + '/' + artist)
-        records = os.listdir(artist_dir)
-        records.sort()
-
-        for record in records:
-
-            record_dir = os.path.join(root + '/' + artist + '/' + record)
-
-            if os.path.isdir(record_dir):
-                cd_list.append([artist, record, record_dir])
+        if os.path.isdir(record_dir):
+            cd_list.append([artist, record, record_dir])
 
     return cd_list
 
-def create_list_artist(artist):
 
-	cd_list = []
-	records = os.listdir(root + '/' + artist)
-	records.sort()
+def get_local_record_list(artist, record):
+    cd_list = dict()
+    record_name = record
+    records = os.listdir(root + '/' + artist)
+    records.sort()
 
-	for record in records:
+    for record in records:
+            record_dir = os.path.join(root + '/' + artist + '/' + record)
 
-		record_dir = os.path.join(root + '/' + artist + '/' + record)
-		
-		if os.path.isdir(record_dir):
-			cd_list.append([artist, record, record_dir])
+            if os.path.isdir(record_dir) and record.lower() == record_name.lower():
+                    cd_list.update({'artist': artist, 'record_name': record, 'record_dir': record_dir})
 
-	return cd_list		
+    return cd_list
 
-def create_list_record(artist, record):
 
-        cd_list = []
-        record_name = record
-        records = os.listdir(root + '/' + artist)
-        records.sort()
+def get_local_record_metadata(artist, album, record_dir):
+    songs = os.listdir(record_dir)
+    album_length = 0
+    track_count = 0
 
-        for record in records:
+    dictionary = dict()
+    dictionary.update({'artist': artist})
+    dictionary.update({'album': album})
 
-                record_dir = os.path.join(root + '/' + artist + '/' + record)
+    for song in songs:
+        path = record_dir + '/' + song
+        extension = pathlib.Path(path).suffix
 
-                if os.path.isdir(record_dir) and record.lower() == record_name.lower():
-                        cd_list.append([artist, record, record_dir])
+        if extension == '.flac':
+            track_count += 1
+            metadata = FLAC(path)
 
-        return cd_list
+            for item in metadata.items():
+                if item[0] == 'tracknumber':
+                    track_number = int(item[1].pop())
 
-def get_artists_by_name(name):
+            min, sec  = divmod(metadata.info.length,60)
+            song_length = "%d:%02d" % (min, sec)
+            dictionary.update({track_number: song_length})
 
-    artists = dict()
+            album_length += metadata.info.length
+
+    min, sec = divmod(album_length, 60)
+    hours, min = divmod(min, 60)
+    album_length = "%d:%02d:%02d" % (hours, min, sec)
+
+    dictionary.update({'album_length': album_length})
+    dictionary.update({'track_count': track_count})
+
+    return dictionary
+
+
+def get_musicbrainz_artists_by_name(name):
+    artists = []
     result = musicbrainzngs.search_artists(artist=name)
 
     for artist in result['artist-list']:
-
         artist_name = artist['name']
         artist_mbid = artist['id']
 
         if artist['ext:score'] == "100":
-            artists.update({artist_mbid:artist_name})
+            artists.append({'artist_mbid': artist_mbid, 'artist_name': artist_name})
 
     return artists
 
-def get_release_group_by_name(artist_id, album):
 
-    records = dict()
+def get_musicbrainz_release_group_by_name(artist_id, album):
+    records = []
     result = musicbrainzngs.search_release_groups(arid=artist_id,release=album,strict=True)
 
     for release_group in result['release-group-list']:
-
         release_group_name = release_group['title']
         release_group_mbid = release_group['id']
-        records.update({release_group_name: release_group_mbid})
+
+        if release_group['ext:score'] == "100":
+            records.append({'release_group_mbid': release_group_mbid, 'release_group_name': release_group_name})
 
     return records
 
-def get_releases(artist_id, release_group_id):
 
-	records = dict()
-	result = musicbrainzngs.search_releases(arid=artist_id,rgid=release_group_id,strict=True)
+def get_musicbrainz_releases(artist_id, release_group_id):
+    releases = []
+    result = musicbrainzngs.search_releases(arid=artist_id,rgid=release_group_id,strict=True)
 
-	for release in result['release-list']:
+    for release in result['release-list']:
+        release_name = release['title']
+        release_mbid = release['id']
+        releases.append({'release_mbid': release_mbid, 'release_name': release_name})
 
-		release_name = release['title']
-		release_mbid = release['id']
-		records.update({release_mbid: release_name})
+    return releases
 
-	return records
 
-def get_record_metadata(artist, album, record_dir):
+def get_musicbrainz_release(record_id):
+    track_count = 0
+    album_length = 0
+    release = musicbrainzngs.get_release_by_id(record_id, includes=['recordings', ])
+    release = release['release']
 
-	songs = os.listdir(record_dir)
-	
-	album_length = 0
+    for recording in release['medium-list']:
+        track_count += int(recording['track-count'])
 
-	dictionary = dict()	
-	dictionary.update({'artist': artist})
-	dictionary.update({'album': album})	
+        for track in recording['track-list']:
+            try:
+                album_length += int(track['track_or_recording_length'])
+            except:
+                album_length += 0
 
-	for song in songs:
+    album_length = album_length / 1000
+    min, sec = divmod(album_length, 60)
+    hours, min = divmod(min, 60)
+    album_length = "%d:%02d:%02d" % (hours, min, sec)
 
-		path = record_dir + '/' + song
-		extension = pathlib.Path(path).suffix
+    # try/except for values that are optional and might not exist
+    try:
+        date = release['date']
+    except KeyError:
+        date = 'unknown'
 
-		if extension == '.flac':
-			metadata = mutagen.File(path)
-			
-			for item in metadata.items():
-				if item[0] == 'tracknumber':
-					track_number = int(item[1].pop())			
+    try:
+        country = release['country']
+    except KeyError:
+        country = 'unknown'
 
-			min, sec  = divmod(metadata.info.length,60)
-			song_length = "%d:%02d" % (min, sec)
-			dictionary.update({track_number: song_length})
+    release_dict = {'date': date, 'id': release['id'], 'medium-count': release['medium-count'],
+                    'country': country, 'track_count': track_count, 'album_length': album_length}
 
-			album_length += metadata.info.length
-	
-	min, sec = divmod(album_length, 60)
-	hours, min = divmod(min, 60)
-	album_length = "%d:%02d:%02d" % (hours, min, sec)
-	dictionary.update({'album length': album_length})
+    return release_dict
 
-	return dictionary
+
+def get_release_suggestions(artist_name, album_name):
+    pp = pprint.PrettyPrinter(indent=10)
+    record = get_local_record_list(artist_name, album_name)
+    release_local = get_local_record_metadata(record['artist'], record['record_name'], record['record_dir'])
+    artists = get_musicbrainz_artists_by_name(artist_name)
+
+    for artist in artists:
+        name = artist['artist_name']
+        mbid = artist['artist_mbid']
+        release_groups = get_musicbrainz_release_group_by_name(mbid, album_name)
+
+        for release_group in release_groups:
+            release_group_name = release_group['release_group_name']
+            release_group_mbid = release_group['release_group_mbid']
+            releases = get_musicbrainz_releases(mbid, release_group_mbid)
+            get_cd_art(mbid, release_group_mbid, record['record_dir'])
+
+            for release in releases:
+                release_musicbrainz = get_musicbrainz_release(release['release_mbid'])
+                if (release_musicbrainz['album_length'] == release_local['album_length'] and
+                    release_musicbrainz['track_count'] == release_local['track_count']):
+                    pp.pprint(release_musicbrainz)
+
+
+def set_release_metadata(artist_name, album_name, record_id):
+    release = musicbrainzngs.get_release_by_id(record_id, includes=['recordings', ])
+    release = release['release']
+    record = get_local_record_list(artist_name, album_name)
+    record_dir = record['record_dir']
+    songs = os.listdir(record_dir)
+
+    artist = artist_name
+    album = release['title']
+    tracktotal = 0
+    for recording in release['medium-list']:
+        tracktotal += int(recording['track-count'])
+    # try/except for values that are optional and might not exist
+    try:
+        date = release['date']
+    except KeyError:
+        date = 'unknown'
+    try:
+        country = release['country']
+    except KeyError:
+        country = 'unknown'
+
+    tracknumber = 1
+    translation = {'/': '-'}
+    table = str.maketrans(translation)
+
+    pic = Picture()
+    with open(f'{record_dir}/fanart.jpg', "rb") as f:
+        pic.data = f.read()
+    pic.mime = u"image/jpeg"
+    pic.width = 1000
+    pic.height = 1000
+    pic.depth = 16 # color depth
+
+    for song in songs:
+        path = record['record_dir'] + '/' + song
+        extension = pathlib.Path(path).suffix
+
+        if extension == '.flac':
+            metadata = FLAC(path)
+
+            for item in metadata.items():
+                if item[0] == 'tracknumber':
+                    tracknumber = int(item[1].pop())
+
+            for recording in release['medium-list']:
+                for track in recording['track-list']:
+                    if int(track['number']) == tracknumber:
+                        tracktitle = track['recording']['title'].translate(table)
+
+            metadata.delete()
+            metadata.add_picture(pic)
+            metadata["Album"] = album
+            metadata["Albumartist"] = artist
+            metadata["Artist"] = artist
+            metadata["Country"] = country
+            metadata["Date"] = date
+            metadata["Title"] = tracktitle
+            metadata["Tracktotal"] = str(tracktotal)
+            metadata["Tracknumber"] = str(tracknumber)
+            metadata.save()
+
+            print(f'Alt: {path}')
+            print(f'Neu: {record_dir}/{artist} - {album} - {tracknumber:02} - {tracktitle}.flac')
+            os.rename(path, f'{record_dir}/{artist} - {album} - {tracknumber:02} - {tracktitle}.flac')
